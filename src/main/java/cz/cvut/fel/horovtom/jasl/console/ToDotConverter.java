03688055543525777ec36664bf961e4f9539c9a9
Tomáš Hořovský <horovtom@fel.cvut.cz>
2018-11-01 17:39:56 +0100

    Making dot conversion work even on ENFA

--- src/main/java/cz/cvut/fel/horovtom/jasl/console/ToDotConverter.java
+++ src/main/java/cz/cvut/fel/horovtom/jasl/console/ToDotConverter.java
@@ -4,6 +4,8 @@ import cz.cvut.fel.horovtom.automata.logic.Automaton;
 import cz.cvut.fel.horovtom.automata.logic.DFAAutomaton;
 import cz.cvut.fel.horovtom.automata.samples.AutomatonSamples;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -28,13 +30,13 @@ public class ToDotConverter {
         String[] q = automaton.getQ();
         res.append("\tnode [shape = doublecircle];\n");
         for (int acceptingStatesIndex : acceptingStatesIndices) {
-            res.append("\t\t").append(q[acceptingStatesIndex]).append("\n");
+            res.append("\t\t\"").append(q[acceptingStatesIndex]).append("\"\n");
         }
 
         res.append("\tnode [shape = circle];\n");
         // Now draw edges:
         for (int i = 0; i < initialStates.length; i++) {
-            res.append("\t\tqS").append(i).append(" -> ").append(q[initialStates[i]]).append(" [color=\"red:invis:red\"];\n");
+            res.append("\t\tqS").append(i).append(" -> \"").append(q[initialStates[i]]).append("\" [color=\"red:invis:red\"];\n");
         }
 
         String[] sigma = automaton.getSigma();
@@ -44,14 +46,18 @@ public class ToDotConverter {
             HashMap<Integer, ArrayList<Integer>> edgesFromState = getEdgesFromState(state, transitions, sigma.length);
             for (int target = 0; target < q.length; target++) {
                 if (!edgesFromState.containsKey(target)) continue;
-                res.append("\t\t").append(q[state]).append(" -> ").append(q[target]).append(" [label = \"");
+                res.append("\t\t\"").append(q[state]).append("\" -> \"").append(q[target]).append("\" [label = \"");
                 ArrayList<Integer> current = edgesFromState.get(target);
                 for (int i = 0; i < current.size(); i++) {
                     String character = sigma[current.get(i)];
+                    if (Automaton.isEpsilonName(character)) character = "&epsilon;";
                     if (i == 0) res.append(character);
                     else res.append(",").append(character);
                 }
-                res.append("\"];\n");
+                if (current.size() == 1 && (sigma[current.get(0)].equals("eps") || sigma[current.get(0)].equals("ε")))
+                    res.append("\", style=dashed];\n");
+                else
+                    res.append("\"];\n");
             }
         }
 
@@ -79,12 +85,12 @@ public class ToDotConverter {
     public static void main(String[] args) {
         //thisIsUgly();
 
-//        try {
-        DFAAutomaton dfaAutomaton = AutomatonSamples.DFASamples.startEndSame();
-        System.out.println(convertToDot(dfaAutomaton));
-//        } catch (FileNotFoundException | UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Automaton dfaAutomaton = AutomatonSamples.ENFASamples.aa_c_a();
+            System.out.println(convertToDot(dfaAutomaton));
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
     }
 
