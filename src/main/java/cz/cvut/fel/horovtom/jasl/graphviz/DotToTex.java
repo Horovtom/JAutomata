6970798cae0c96ba578550c220ea5d64e1176b2a
Tomáš Hořovský <horovtom@fel.cvut.cz>
2018-12-03 23:07:09 +0100

    Change image scaling to preserve aspect ratio of the final TIKZ image

--- src/main/java/cz/cvut/fel/horovtom/jasl/graphviz/DotToTex.java
+++ src/main/java/cz/cvut/fel/horovtom/jasl/graphviz/DotToTex.java
@@ -10,6 +10,7 @@ import guru.nidi.graphviz.parse.Parser;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 public class DotToTex {
@@ -26,22 +27,49 @@ public class DotToTex {
         N, W, S, E
     }
 
-    private class Coordinate<T> {
-        T X;
-        T Y;
+    private static class Coordinate {
+        double x;
+        double y;
 
-        Coordinate(T x, T y) {
-            this.X = x;
-            this.Y = y;
+        Coordinate(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        double magnitude() {
+            return Math.sqrt(x * x + y * y);
+        }
+
+        void mult(double scalar) {
+            x *= scalar;
+            y *= scalar;
         }
 
         @Override
         public String toString() {
-            final StringBuffer sb = new StringBuffer("Coordinate{");
-            sb.append("X=").append(X);
-            sb.append(", Y=").append(Y);
-            sb.append('}');
-            return sb.toString();
+            return "Coordinate{" + "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+
+        static Coordinate subtract(Coordinate a, Coordinate b) {
+            return new Coordinate(a.x - b.x, a.y - b.y);
+        }
+
+        static Coordinate add(Coordinate a, Coordinate b) {
+            return new Coordinate(a.x - b.x, a.y - b.y);
+        }
+
+        static Coordinate mult(Coordinate a, double scalar) {
+            return new Coordinate(a.x * scalar, a.y * scalar);
+        }
+
+        static Coordinate mult(Coordinate a, Coordinate b) {
+            return new Coordinate(a.x * b.x, a.y * b.y);
+        }
+
+        public static double dot(Coordinate a, Coordinate b) {
+            return a.x * b.x + a.y * b.y;
         }
     }
 
@@ -51,11 +79,11 @@ public class DotToTex {
     /**
      * Bottom left corner of tikz image
      */
-    private final Coordinate<Integer> tikzMin = new Coordinate<>(0, 0);
+    private final Coordinate tikzMin = new Coordinate(0, 0);
     /**
      * Top right corner of tikz image
      */
-    private final Coordinate<Integer> tikzMax = new Coordinate<>(12, 16);
+    private final Coordinate tikzMax = new Coordinate(11, 14);
 
     // endregion
 
@@ -81,7 +109,7 @@ public class DotToTex {
 
 
         String s = GraphvizAPI.toFormattedDot(a);
-        System.out.println(GraphvizAPI.toDot(a));
+        System.out.println(s);
         GraphvizAPI.dotToPNG(a, "test.png", "test2.png", "test3.png");
 
         DotToTex dtt = new DotToTex();
@@ -91,114 +119,67 @@ public class DotToTex {
     }
 
     private String convertToTex(Automaton automaton, String dotCode) throws IOException {
+        final int qSize = automaton.getQSize();
+        HashMap<String, Integer> inverseQ = getInverseQ(automaton);
 
+
+        // This holds the graph parsed from the dot file.
         MutableGraph mg = Parser.read(dotCode);
-        MutableNode[] nodes = mg.nodes().toArray(new MutableNode[0]);
-        HashMap<Integer, Link[]> edges = new HashMap<>();
-        HashMap<Integer, HashMap<Integer, ArrayList<Coordinate<Double>>>> edgesAttributes = new HashMap<>();
-
-        String[] Q = automaton.getQ();
-        HashMap<String, Integer> inverseQ = new HashMap<>();
-        for (int i = 0; i < Q.length; i++) {
-            inverseQ.put(Q[i], i);
-        }
-
-        HashMap<Integer, MutableNode> nodesMap = new HashMap<>();
-
-        for (MutableNode node : nodes) {
-            HashMap<Integer, ArrayList<Coordinate<Double>>> currEdges = new HashMap<>();
-            Link[] links = node.links().toArray(new Link[0]);
-            for (Link link : links) {
-                MutableNode to = ((MutableNode) link.to().asLinkSource());
-                int targ = inverseQ.get(to.name().value());
-
-                ArrayList<Coordinate<Double>> points = new ArrayList<>();
-                String pos = (String) link.attrs().get("pos");
-                if (pos.startsWith("e,")) pos = pos.substring(2, pos.length());
-                String[] pairs = pos.split(" ");
-                for (String pair : pairs) {
-                    String[] split = pair.split(",");
-                    points.add(new Coordinate<>(Double.parseDouble(split[0]), Double.parseDouble(split[1])));
-                }
-                currEdges.put(targ, points);
-
-            }
-
-            edgesAttributes.put(inverseQ.get(node.name().value()), currEdges);
-
-            nodesMap.put(inverseQ.get(node.name().toString()), node);
-            edges.put(inverseQ.get(node.name().value()), links);
-        }
-
-        int[] initialStates = automaton.getInitialStates();
-        int[] acceptingStates = automaton.getAcceptingStates();
-
+        // Form our parsed dot file we will need the bounding box:
         String[] bb = ((String) mg.graphAttrs().get("bb")).split(",");
-//        Coordinate<Integer> min = new Coordinate<>(Integer.parseInt(bb[0]), Integer.parseInt(bb[1]));
-//        Coordinate<Integer> max = new Coordinate<>(Integer.parseInt(bb[2]), Integer.parseInt(bb[3]));
+        // And the description for all the nodes
+        MutableNode[] nodes = mg.nodes().toArray(new MutableNode[0]);
+        // We will hold mapping to nodes indices to those nodes for later use
+        HashMap<Integer, MutableNode> nodesMap = new HashMap<>();
+        // And we will hold node coordinates in this array, indexed by Q
+        Coordinate[] coordinates;
 
-        Coordinate<Integer> min = new Coordinate<>(0, 0);
-        Coordinate<Integer> max = new Coordinate<>(576, 216);
+        // We want to get edges attributes from those nodes.
+        HashMap<Integer, HashMap<Integer, ArrayList<Coordinate>>> edgesAttributes = new HashMap<>();
+
+        getEdgesAttributes(edgesAttributes, nodesMap, nodes, inverseQ);
+
+        coordinates = getNodeCoordinates(nodesMap, qSize);
+
+        // Get real bounding box of the image
+        Coordinate min = new Coordinate(Double.MAX_VALUE, Double.MAX_VALUE);
+        Coordinate max = new Coordinate(Double.MIN_VALUE, Double.MIN_VALUE);
+        for (Coordinate coordinate : coordinates) {
+            if (coordinate.x < min.x) min.x = coordinate.x;
+            if (coordinate.y < min.y) min.y = coordinate.y;
+            if (coordinate.x > max.x) max.x = coordinate.x;
+            if (coordinate.y > max.y) max.y = coordinate.y;
+        }
+
+        // This contains tikz coordinates of the nodes.
+        Coordinate[] newCoordinates = getScaledCoordinates(coordinates, min, max);
 
         // Now try to assemble the output
         StringBuilder sb = new StringBuilder("\\begin{tikzpicture}[->,>=stealth',shorten >=1pt,auto,node distance=2.8cm,semithick,initial text=$ \t$]\n");
 
-        ArrayList<Coordinate<Double>> coordinates = new ArrayList<>(Q.length);
-        // Nodes:
-
-        for (int i = 0; i < Q.length; i++) {
-            sb.append("\t\\node[state");
-            for (int initialState : initialStates) {
-                if (initialState == i) {
-                    sb.append(", initial, initial where=left");
-
-                    break;
-                }
-            }
-
-            for (int acceptingState : acceptingStates) {
-                if (acceptingState == i) {
-                    sb.append(", accepting");
-                }
-            }
-
-            sb.append("] (").append(i).append(") at (");
-
-            MutableNode currNode = nodesMap.get(i);
-            String posString = (String) currNode.attrs().get("pos");
-            String[] posSplit = posString.split(",");
-            Coordinate<Double> pos = new Coordinate<>(Double.parseDouble(posSplit[0]), Double.parseDouble(posSplit[1]));
-            coordinates.add(pos);
-
-            // Now normalize for tikz output
-            pos = mapToTikz(min, max, pos);
-
-            sb.append(String.format("%.2f", pos.X)).append(",").append(String.format("%.2f", pos.Y));
-            sb.append(") {$").append(Q[i]).append("$};\n");
-        }
-
+        addNodesToSb(sb, automaton, newCoordinates);
 
         // Edges:
         sb.append("\t\\path");
 
 
-        for (int i = 0; i < Q.length; i++) {
+        for (int i = 0; i < qSize; i++) {
             HashMap<Integer, String> curr = getEdgesFromState(automaton, i);
             if (curr.size() == 0) continue;
             sb.append("\n\t\t(").append(i).append(")\n");
-            Coordinate<Double> source = coordinates.get(i);
+            Coordinate source = coordinates[i];
 
             for (Integer target : curr.keySet()) {
                 String edgeLabel = curr.get(target);
 
                 sb.append("\n\t\t\tedge ");
 
-                ArrayList<Coordinate<Double>> edgeAttrs = edgesAttributes.get(i).get(target);
+                ArrayList<Coordinate> edgeAttrs = edgesAttributes.get(i).get(target);
 
                 //FIXME: For now try the middle of the points:
 
 
-                Coordinate<Double> middlePoint = edgeAttrs.get((int) Math.floor(edgeAttrs.size() / 2));
+                Coordinate middlePoint = edgeAttrs.get((int) Math.floor(edgeAttrs.size() / 2));
                 String edgeProperties;
 
                 if (target == i) {
@@ -208,7 +189,7 @@ public class DotToTex {
                 } else {
                     // Now... How does it curve?
 
-                    int angle = getCurveAngle(source, coordinates.get(target), middlePoint);
+                    int angle = getCurveAngle(source, coordinates[target], edgeAttrs);
                     if (angle < 0) {
                         // We have bend right
                         edgeProperties = "[bend right = " + Math.abs(angle) + "]";
@@ -239,8 +220,114 @@ public class DotToTex {
         // Now end our misery
         sb.append(";\n\\end{tikzpicture}\n");
 
-
         return sb.toString();
+    }
+
+    private Coordinate[] getScaledCoordinates(Coordinate[] coordinates, Coordinate min, Coordinate max) {
+        // We use scaling that will preserve aspect ratio of the image.
+        
+        Coordinate size = Coordinate.subtract(max, min);
+
+        // Either by X:
+        double ratio = tikzMax.x / size.x;
+        double yMax = size.y * ratio;
+        if (yMax > tikzMax.y) {
+            // By Y:
+            ratio = tikzMax.y / size.y;
+            double xMax = size.x * ratio;
+        }
+
+        Coordinate[] scaled = new Coordinate[coordinates.length];
+        for (int i = 0; i < coordinates.length; i++) {
+            scaled[i] = Coordinate.mult(coordinates[i], ratio);
+        }
+        return scaled;
+    }
+
+    private Coordinate[] getNodeCoordinates(HashMap<Integer, MutableNode> nodesMap, int qSize) {
+        Coordinate[] coordinates = new Coordinate[qSize];
+        for (int i = 0; i < qSize; i++) {
+            final MutableNode node = nodesMap.get(i);
+            String pos = (String) node.attrs().get("pos");
+            String[] posSplit = pos.split(",");
+            Coordinate position = new Coordinate(Double.parseDouble(posSplit[0]), Double.parseDouble(posSplit[1]));
+            coordinates[i] = position;
+        }
+
+        return coordinates;
+    }
+
+    private void addNodesToSb(StringBuilder sb,
+                              Automaton automaton,
+                              Coordinate[] coordinates) {
+
+        final int[] initialStates = automaton.getInitialStates();
+        final int[] acceptingStates = automaton.getAcceptingStates();
+        final String[] Q = automaton.getQ();
+        final int qSize = Q.length;
+
+        for (int i = 0; i < qSize; i++) {
+            sb.append("\t\\node[state");
+            for (int initialState : initialStates) {
+                if (initialState == i) {
+                    sb.append(", initial, initial where=left");
+
+                    break;
+                }
+            }
+
+            for (int acceptingState : acceptingStates) {
+                if (acceptingState == i) {
+                    sb.append(", accepting");
+                }
+            }
+
+            sb.append("] (").append(i).append(") at (");
+            Coordinate pos = coordinates[i];
+
+            sb.append(String.format(Locale.US, "%.2f", pos.x)).append(",").append(String.format(Locale.US, "%.2f", pos.y));
+            sb.append(") {$").append(Q[i]).append("$};\n");
+        }
+    }
+
+    private void getEdgesAttributes(HashMap<Integer, HashMap<Integer, ArrayList<Coordinate>>> edgesAttributes,
+                                    HashMap<Integer, MutableNode> nodesMap,
+                                    MutableNode[] nodes, HashMap<String, Integer> inverseQ) {
+
+        for (MutableNode node : nodes) {
+            HashMap<Integer, ArrayList<Coordinate>> currEdges = new HashMap<>();
+            Link[] links = node.links().toArray(new Link[0]);
+            for (Link link : links) {
+                MutableNode to = ((MutableNode) link.to().asLinkSource());
+                int targ = inverseQ.get(to.name().value());
+
+                ArrayList<Coordinate> points = new ArrayList<>();
+                String pos = (String) link.attrs().get("pos");
+                if (pos.startsWith("e,")) pos = pos.substring(2, pos.length());
+                String[] pairs = pos.split(" ");
+                for (String pair : pairs) {
+                    // FIXME: This check in here is because in a bug in graphviz-java, where it cannot parse too long lines
+                    if (pair.startsWith("\\\r\n")) pair = pair.substring(3, pair.length());
+                    String[] split = pair.split(",");
+                    points.add(new Coordinate(Double.parseDouble(split[0]), Double.parseDouble(split[1])));
+                }
+                currEdges.put(targ, points);
+
+            }
+
+            edgesAttributes.put(inverseQ.get(node.name().value()), currEdges);
+
+            nodesMap.put(inverseQ.get(node.name().toString()), node);
+        }
+    }
+
+    private HashMap<String, Integer> getInverseQ(Automaton automaton) {
+        String[] Q = automaton.getQ();
+        HashMap<String, Integer> inverseQ = new HashMap<>();
+        for (int i = 0; i < Q.length; i++) {
+            inverseQ.put(Q[i], i);
+        }
+        return inverseQ;
     }
 
 
@@ -251,18 +338,16 @@ public class DotToTex {
      * @param middlePoint Coordinate of the middle point of the edge curve
      * @return String containing one of these options: "above", "below", "left", "right"
      */
-    private String getLoopDirection(Coordinate<Double> node, Coordinate<Double> middlePoint) {
-        //FIXME: Test this
-        double xDiff = node.X - middlePoint.X;
-        double yDiff = node.Y - middlePoint.Y;
-        if (xDiff > 0) {
-            if (yDiff > 0) {
+    private String getLoopDirection(Coordinate node, Coordinate middlePoint) {
+        Coordinate diff = Coordinate.subtract(node, middlePoint);
+        if (diff.x > 0) {
+            if (diff.x > 0) {
                 return "below";
             } else {
                 return "left";
             }
         } else {
-            if (yDiff > 0) {
+            if (diff.y > 0) {
                 return "right";
             } else {
                 return "above";
@@ -271,27 +356,35 @@ public class DotToTex {
     }
 
 
+    // TODO: MAKE THIS FUNCTION BETTER!
     /**
      * This function will return the angle of start of the curve. It will be positive, if the curve is to the left,
      * or negative if the curve is to the right.
      *
      * @param source      Coordinate of the source node
      * @param dest        Coordinate of the destination node
-     * @param middlePoint Coordinate of the middle point of the edge curve
+     * @param curvePoints Coordinates of anchor points of the curve spline
      * @return integer containing the angle of the start of the curve.
      */
-    private int getCurveAngle(Coordinate<Double> source, Coordinate<Double> dest, Coordinate<Double> middlePoint) {
+    private int getCurveAngle(Coordinate source, Coordinate dest, ArrayList<Coordinate> curvePoints) {
+        Coordinate a = Coordinate.subtract(dest, source);
 
-        Coordinate<Double> a = new Coordinate<>(dest.X - source.X, dest.Y - source.Y);
-        Coordinate<Double> b = new Coordinate<>(middlePoint.X - source.X, middlePoint.Y - source.Y);
+        double maxAngle = Double.MIN_VALUE;
+        int crossOfMax = 0;
+        for (Coordinate curvePoint : curvePoints) {
+            Coordinate b = Coordinate.subtract(curvePoint, source);
 
-        double numerator = a.X * b.X + a.Y * b.Y;
-        double denominator = Math.sqrt(a.X * a.X + a.Y * a.Y) * Math.sqrt(b.X * b.X + b.Y * b.Y);
-        double angle = Math.acos(numerator / denominator);
-        angle = angle * (180 / Math.PI);
-        int cross = (int) Math.signum(a.X * b.Y - a.Y * b.X);
+            double numerator = Coordinate.dot(a, b);
+            double denominator = a.magnitude() * b.magnitude();
+            double angle = Math.acos(numerator / denominator);
+            angle = angle * (180 / Math.PI);
+            if (angle > maxAngle) {
+                maxAngle = angle;
+                crossOfMax = (int) Math.signum(a.x * b.y - a.y * b.x);
+            }
+        }
 
-        return (int) Math.round(angle) * cross;
+        return (int) Math.round(maxAngle) * crossOfMax;
     }
 
 
@@ -302,10 +395,10 @@ public class DotToTex {
         return newMin + ((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin);
     }
 
-    private Coordinate<Double> mapToTikz(Coordinate<Integer> dotMin, Coordinate<Integer> dotMax, Coordinate<Double> pos) {
-        double x = map(pos.X, dotMin.X, dotMax.X, tikzMin.X, tikzMax.X);
-        double y = map(pos.Y, dotMin.Y, dotMax.Y, tikzMin.Y, tikzMax.Y);
-        return new Coordinate<>(x, y);
+    private Coordinate mapToTikz(Coordinate dotMin, Coordinate dotMax, Coordinate pos) {
+        double x = map(pos.x, dotMin.x, dotMax.x, tikzMin.x, tikzMax.x);
+        double y = map(pos.y, dotMin.y, dotMax.y, tikzMin.y, tikzMax.y);
+        return new Coordinate(x, y);
     }
 
     /**
